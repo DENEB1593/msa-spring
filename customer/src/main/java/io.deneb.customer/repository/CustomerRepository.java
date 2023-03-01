d64c49f561815aeb83b44ad9c02323b90a872560
deneb1593 <deneb1593@gmail.com>
2023-03-01 17:28:35 +0900

    DB 추가 / Customer 저장

---
+++ customer/src/main/java/io.deneb.customer/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package io.deneb.customer.repository;
+
+import io.deneb.customer.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository
+  extends JpaRepository<Customer, Integer> {
+}
--- customer/src/main/java/io.deneb.customer/service/CustomerService.java
+++ customer/src/main/java/io.deneb.customer/service/CustomerService.java
@@ -2,15 +2,19 @@ package io.deneb.customer.service;
 
 import io.deneb.customer.controller.CustomerRegistration;
 import io.deneb.customer.model.Customer;
+import io.deneb.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(
+  CustomerRepository customerRepository) {
   public void register(CustomerRegistration registration) {
     Customer customer = Customer.builder()
       .firstName(registration.firstName())
       .lastName(registration.lastName())
       .email(registration.email())
       .build();
+
+    customerRepository.save(customer);
   }
 }
