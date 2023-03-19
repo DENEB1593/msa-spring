3669b8d297ad7f0c77234cec6f45addd78045c64
deneb1593 <deneb1593@gmail.com>
2023-03-19 19:44:17 +0900

    customer, fraud, notification 모듈 k8s 기반 통신 추가

--- notification/src/main/java/io/deneb/notification/Application.java
+++ notification/src/main/java/io/deneb/notification/Application.java
@@ -1,12 +1,10 @@
 package io.deneb.notification;
 
-import io.deneb.amqp.config.RabbitMQMessageProducer;
-import io.deneb.notification.config.NotificationConfig;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
   scanBasePackages = {
@@ -15,23 +13,13 @@ import org.springframework.context.annotation.Bean;
   }
 )
 @EnableEurekaClient
+@PropertySources({
+    @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
 
-  @Bean
-  CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer,
-                                      NotificationConfig notificationConfig) {
-    return args -> {
-      producer.publish(
-        new Person("deneb", 30),
-        notificationConfig.getInternalExchange(),
-        notificationConfig.getInternalNotificationRoutingKey()
-      );
-    };
-  }
-
-  record Person(String name, int age) { }
 
 }
